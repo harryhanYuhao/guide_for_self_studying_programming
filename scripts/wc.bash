8edf43385dd72806c1b3fe7efbffcae728477158
Harry Han <s2162783@ed.ac.uk>
2024-10-22 09:26:54 +0100

    wc.bash now only records the newest word count for each day

--- scripts/wc.bash
+++ scripts/wc.bash
@@ -12,9 +12,22 @@ if [[ $(pwd) == *"scripts"* ]]; then
 	SOURCE_FILE_PATH="../src/"
 fi
 
-WORD_COUNT=`find $SOURCE_FILE_PATH -type f | xargs wc -w | tail -n 1 | awk '{print $1;}'`
 DATE=`date`
 
+# The output is of the form "Tue 22 OCT HH:MM:SS BST 2024"
+# get the first 10 characters
+TODAY=${DATE:0:10}
+# Check day of last word count
+LAST_DATE=`tail -n 1 $LOG_FILE_PATH`
+LAST_DATE=${LAST_DATE:0:10}
+
+# if match, delete the last line
+if [ "$TODAY" == "$LAST_DATE" ]; then
+	sed -i '$d' $LOG_FILE_PATH
+fi
+
+# get and append the new word count
+WORD_COUNT=`find $SOURCE_FILE_PATH -type f | xargs wc -w | tail -n 1 | awk '{print $1;}'`
 echo "${DATE}, ${WORD_COUNT}" >> $LOG_FILE_PATH
 
 # modified pre-commit, add to the pending commit
